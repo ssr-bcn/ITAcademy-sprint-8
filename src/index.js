bf6f203efbf6c81c5e874c4ce408f8658abeff9f
SSR <ssr@estamosenello.net>
2022-05-15 12:38:03 +0200

    Modificado el modo de cargar las fuentes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import '@fontsource/barlow';
+import '@fontsource/barlow/100.css';
+import '@fontsource/barlow/700.css';
 import './index.css';
 import App from './App';
 
